d052837143451af9824af9302cab10db5480df2c
Yuichi Yogo <yuichkun@gmail.com>
2019-03-28 04:27:28 +0900

    improve store management

--- node_content/app.js
+++ node_content/app.js
@@ -15,6 +15,7 @@ class App {
   }
 
   async initialize() {
+    console.log('prev store state');
     const port = await portscanner.findAPortNotInUse(3000, 9000).catch(console.error);
     const server = io.listen(port);
     server.on("connection", function(socket) {
@@ -22,15 +23,6 @@ class App {
       socket.emit("welcome", "welcome man");
     });
 
-    const store = createStore(reducers);
-    /**
-     * @TODO test this
-     */
-    store.subscribe(() => {
-      this.maxAPI.outlet(store.getState());
-    });
-
-    this.store = store;
     this.server = server;
     this.openBrowser = () => {
       openBrowser(port);
@@ -38,6 +30,26 @@ class App {
     this.closeBrowser = () => {
       server.emit('broadcast', 'CLOSE');
     };
+  }
+
+  initStore(initialDictState) {
+    const store = createStore(reducers, initialDictState);
+    this.maxAPI.outlet(store.getState());
+    store.subscribe(() => {
+      this.maxAPI.outlet(store.getState());
+    });
+    this.store = store;
+  }
+
+  dispatch(...args) {
+    const [type] = args;
+    const payload = args.slice(1);
+    const action = {
+      type,
+      payload
+    };
+    console.log(action);
+    this.store.dispatch(action);
   }
 
 }
