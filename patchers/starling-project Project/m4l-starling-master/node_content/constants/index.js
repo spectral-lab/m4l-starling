8644e44b64ec6745c7ec4bf7d8b34a782a426562
Yuichi Yogo <yuichkun@gmail.com>
2019-04-11 21:04:56 +0900

    reorganize folder structure

--- patchers/starling-project Project/m4l-starling-master/node_content/constants/index.js
+++
@@ -1,4 +0,0 @@
-module.exports = {
-  // STARLING_DOMAIN_URL: "http://localhost:8080"
-  STARLING_DOMAIN_URL: "http://pwt-extractor.com"
-};
--- patchers/starling-project Project/m4l-starling-master/node_content/reducers/index.js
+++
@@ -1,8 +0,0 @@
-const { combineReducers } = require('redux');
-const M4L = require('./m4l');
-const pwt = require('./pwt');
-
-module.exports = combineReducers({
-  M4L,
-  pwt
-});
